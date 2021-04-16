15af24606ecc8cf55d4c91e5cfa6d86c81976eb9
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-04-16 17:16:47 +0200

    Feat: created clients table, model, controller and routes

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,19 @@
+class ClientsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+end
