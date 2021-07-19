6612c2445f76fc604416c351b5ff21bae7a46d41
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-07-19 20:55:57 +0200

    Feat: client data brick tests created

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @client = Client.create(name: "Daniel", email: "daniel@eltacolab.com", contact_name: "daniel")
+  end
+
+  test "Client should be valid" do
+    assert @client.valid?
+    # assert true
+  end
+
+  test "Client name should not be blank" do
+    @client.name = ""
+    assert_not @client.valid?
+  end
+
+  test "Email should be valid" do
+    @client.email.include?("@")
+    assert @client.valid?
+  end
 end
